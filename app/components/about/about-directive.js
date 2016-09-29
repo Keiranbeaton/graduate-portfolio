1c7b84e8af9de0123c7f6255724cbdd706e62afa
keiran beaton <keiran beaton>
2016-09-29 16:54:05 -0700

    about page looking decent

--- app/components/about/about-directive.js
+++ app/components/about/about-directive.js
@@ -4,10 +4,7 @@ module.exports = (app) => {
   app.directive('kbAbout', function() {
     return {
       restrict: 'EAC',
-      controller: 'AboutController',
       template: require('./about-template.html'),
-      bindToController: true,
-      scope: {}
     };
   });
 };
