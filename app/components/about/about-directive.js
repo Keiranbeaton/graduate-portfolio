dc6515e53f156c5114789d5dcddd8889af609ffe
keiran beaton <keiran beaton>
2016-09-28 19:39:44 -0700

    done for the day

--- app/components/about/about-directive.js
+++ app/components/about/about-directive.js
@@ -3,6 +3,7 @@
 module.exports = function(app) {
   app.directive('kbAbout', function() {
     return {
+      restrict: 'EAC',
       controller: 'AboutController',
       templateUrl: './about-template.html',
       bindToController: true,
