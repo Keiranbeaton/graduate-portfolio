b243e981d2122f381156870b28db227743ca335a
keiran beaton <keiran beaton>
2016-10-04 12:28:50 -0700

    tentative final product

--- assets/projects.js
+++ assets/projects.js
@@ -7,16 +7,14 @@ module.exports = [{
   displayName: 'dont-stop-retrievin',
   url: 'https://dont-stop-retrieving.herokuapp.com/',
   githubUrl: 'https://github.com/Keiranbeaton/dont-stop-retrieving',
-  imageMain: '../../../../assets/dsr-main.png',
-  imageSecondary: '../../../../assets/dsr-single.png'
+  images: [{url: '../../../../assets/dsr-main.png', text: 'Main Page View'}, {url: '../../../../assets/dsr-single.png', text: 'Single Park View'}]
 },
 {
   name: 'Sports Messenger',
-  title: 'A single page web app that allows residents of Seattle to find Seattle parks to play pick up sports at.',
+  title: 'A single page web app that allows residents of Seattle to find Seattle parks to play pick up sports at. We were working under time constraints so this app is still a work in progress.',
   description: 'This page was the final group project I completed at Code Fellows. I was one of three people in the group, and worked mostly on Javascript, both frontend and backend. Our goal was to create an app that allowed users to search local parks based on their sporting facilities, and to rate and comment on these parks so other users would be able to find the level of competition they were looking for. We also implemented a sign up and sign in function so that only registered users could view the site, and leave comments and ratings.',
   displayName: 'sports-messenger',
   url: 'https://stormy-fjord-91599.herokuapp.com/#/home',
   githubUrl: 'https://github.com/sports-messenger/sports-messenger',
-  imageMain: '../../../../assets/sm-home.png',
-  imageSecondary: '../../../../assets/sm-signin.png'
+  images: [{url: '../../../../assets/sm-home.png', text: 'Main Page View'}, {url:'../../../../assets/sm-signin.png', text: 'Sign In View'}]
 }];
